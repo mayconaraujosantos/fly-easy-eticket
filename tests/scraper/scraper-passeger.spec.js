a3535b46847cb70dc4a113d90b0d0521bbb315ca
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-03-10 19:00:07 -0300

    test: ensure scraper to collect arrival ticket

--- tests/scraper/scraper-passeger.spec.js
+++ tests/scraper/scraper-passeger.spec.js
@@ -1,30 +1,36 @@
-import scraperPassenger from '../../src/scraper/collector/scraper-passenger';
+import { grabPassenger } from '../../src/scraper/collector/passenger';
 import startBrowser from '../../src/scraper/config/browser';
-import CODE from '../../src/utils/code-url-list';
 import { pageBrowserClosed } from '../../src/utils/page-browser-utils';
-import { fakePassenger, fakePassengerNegative } from '../fixture';
+import { PASSENGERS } from '../fixture/passergers';
 
 let page, browser, passenger;
+
+const CODE_RIDPUU = '5c79b9fa2334f476f6147457a34357efc3fa31db';
 
 beforeAll(async () => {
 	browser = await startBrowser();
 	page = await browser.newPage();
-	passenger = await scraperPassenger(CODE[0]);
+	await page.goto('https://voarfacil.net/eticket/' + CODE_RIDPUU, {
+		waitUntil: 'networkidle2',
+	});
+
+	passenger = await grabPassenger(page);
 });
 
 afterAll(async () => {
 	await pageBrowserClosed(page, browser);
 });
 
-describe('Passenger information', () => {
-	it('Should be return a passenger with code reservation and board pass', async () => {
-		expect(passenger[0].code).toBe(fakePassenger[0].code);
-		expect(passenger[0].codeReservation).toBe(fakePassenger[0].codeReservation);
+describe('Get information abount issuing your ticket', () => {
+	// positive scenario
+	// Must return a ticket with the departure flight information
+	it('Must return a ticket with the departure flight information', async () => {
+		expect(passenger).toStrictEqual(PASSENGERS);
 	});
-	it('Should be return a passenger with not equal code reservation and not equal board pass', async () => {
-		expect(passenger[0].code).not.toBe(fakePassengerNegative[0].code);
-		expect(passenger[0].codeReservation).not.toBe(
-			fakePassengerNegative[0].codeReservation,
-		);
-	});
+	// it('Must return a ticket with the arrival flight information', async () => {
+	// 	expect(passenger).toStrictEqual(PASSENGERS);
+	// });
+	// it('Must return a ticket with the connection flight information', async () => {
+	// 	expect(passenger).toStrictEqual(PASSENGERS);
+	// });
 });
