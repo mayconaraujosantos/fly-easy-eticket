805c95f6d71c333ada60fc5bfba079ce98360da9
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2023-12-25 17:23:27 -0300

    feat: create scraper to collect tickets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import logger from '@common/index';
-import { TravelScraper } from '@scraper/travel-scraper';
+import { TravelScraper } from '@infra/puppeteer/trip-scraper';
 
-const eticketCode = '329e5890ef8603ae36e50e1e1a09a0bc42d6133f';
+// const eticketCode = '7d000a04ab62ef2b889e10a3bf8c7001885ec3db'; //7 passageiros
+const eticketCode = '1cffdeb03abff1346e6148c6d46376c8e0eb9d6e'; // somente ida
+// const eticketCode = '5d99e08e8564df60d862b85174455acc7c8ff0f8'; // somente ida
 
 async function main() {
 	try {
 		const travelData = await TravelScraper.scrapeTravelData(eticketCode);
-		console.log(travelData);
+
+		const printTravelData = JSON.stringify(travelData, null, 2);
+		console.log(printTravelData);
 	} catch (error) {
 		logger.error(error);
 	}
---
+++ src/infra/puppeteer/trip-scraper.js
@@ -0,0 +1,179 @@
+import logger from '@common/index';
+import { ScraperUtils } from './scraper-utils';
+import { PASSENGER, TRIPS } from '@common/constant/selectors';
+
+export class TravelScraper {
+	static async collectTravelData(page) {
+		return await page.evaluate(
+			(TRIPS, PASSENGER) => {
+				const getInnerTextOrNullOrUndefined = (element, selector) => {
+					const selectedElement = element.querySelector(selector);
+					return selectedElement ? selectedElement.innerText : null;
+				};
+
+				const extractPassengerData = (passenger) => {
+					const departure = [
+						{
+							dateOutBound: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.DATE_OUT_BOUND,
+							),
+							departureTime: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.TIME,
+							)?.replace(':', ''),
+							departureCity: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.CITY,
+							),
+							departureAirport: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.AIRPORT,
+							),
+						},
+					];
+
+					const airlineTicket = [
+						{
+							airlineCia: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.AIRLINE_CIA,
+							),
+							flightNumber: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.FLIGHT_NUMBER,
+							)
+								?.replace('voo', '')
+								.trim(),
+							classService: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.CLASS_SERVICE,
+							),
+							flightDuration: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.FLIGHT_DURATION,
+							)
+								?.replace(/duração/g, '')
+								.replace(/\D/g, '')
+								.trim(),
+						},
+					];
+
+					const arrival = [
+						{
+							arrivalTime: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.ARRIVAL_TIME,
+							),
+							arrivalLocation: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.ARRIVAL_LOCATION,
+							),
+							arrivalAirport: getInnerTextOrNullOrUndefined(
+								passenger,
+								TRIPS.ARRIVAL_AIRPORT,
+							),
+						},
+					];
+
+					const connection = [
+						...passenger.querySelectorAll('div.eticket__stop-body'),
+					]
+						.flatMap((interruption, index) => {
+							if (index === 1 || index === 2) {
+								return [
+									{
+										date: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.DATE_OUT_BOUND,
+										),
+										time: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.TIME,
+										),
+										city: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.CITY,
+										),
+										airport: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.AIRPORT,
+										),
+										airlineCia: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.AIRLINE_CIA,
+										),
+										flightNumber: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.FLIGHT_NUMBER,
+										),
+										classService: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.CLASS_SERVICE,
+										),
+										flightDuration: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.FLIGHT_DURATION,
+										),
+										arrivalCity: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.ARRIVAL_LOCATION,
+										),
+										arrivalTime: getInnerTextOrNullOrUndefined(
+											interruption,
+											TRIPS.ARRIVAL_TIME,
+										),
+									},
+								];
+							}
+						})
+						.filter((elements) => elements != null);
+
+					const passengers = [
+						...passenger.querySelectorAll(TRIPS.ARRIVAL_PASSENGER),
+					].map((p) => ({
+						firstName: getInnerTextOrNullOrUndefined(p, PASSENGER.FIRST_NAME),
+						lastName: getInnerTextOrNullOrUndefined(p, PASSENGER.LAST_NAME),
+						pnr: getInnerTextOrNullOrUndefined(p, PASSENGER.PNR),
+						eticket: getInnerTextOrNullOrUndefined(p, PASSENGER.ETICKET),
+						buyer: getInnerTextOrNullOrUndefined(p, PASSENGER.BUYER),
+					}));
+
+					return {
+						trips: getInnerTextOrNullOrUndefined(passenger, TRIPS.TRIP),
+						passengers: Array.from(new Set(passengers.map(JSON.stringify))).map(
+							JSON.parse,
+						),
+						departures: departure,
+						airlineTickets: airlineTicket,
+						arrivals: arrival,
+						connections: connection,
+						totalFlightDuration: getInnerTextOrNullOrUndefined(
+							passenger,
+							TRIPS.TOTAL_FLIGHT_DURATION,
+						),
+					};
+				};
+
+				const passengerData = [
+					...document.querySelectorAll(TRIPS.ETICKET_CARD_BODY),
+				].map((passenger) => extractPassengerData(passenger));
+
+				return passengerData;
+			},
+			TRIPS,
+			PASSENGER,
+		);
+	}
+
+	static async scrapeTravelData(code) {
+		const { page, browser } = await ScraperUtils.initializeBrowser(code);
+		try {
+			return await this.collectTravelData(page);
+		} catch (error) {
+			logger.error(error);
+		} finally {
+			await ScraperUtils.closeBrowser(page, browser);
+		}
+	}
+}
