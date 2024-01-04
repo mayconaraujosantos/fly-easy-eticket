528f9ad675f9a18373469ca0c753fc3ac8bb0978
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-01-04 00:09:36 -0300

    chore: add package-logs dependencies and settings config

--- tests/unit/verify-ticket.spec.js
+++ tests/unit/verify-ticket.spec.js
@@ -1,10 +1,19 @@
+import { MissingParamError } from '../../src/utils/errors';
 import { TicketValidator } from '../../src/utils/helpers/ticket-validator';
 
 describe('TicketValidator', () => {
+	const ticketValidator = new TicketValidator();
+
 	it('should return true for a valid ticket code', () => {
-		const ticketValidator = new TicketValidator();
 		const validCode = '1cffdeb03abff1346e6148c6d46376c8e0eb9d6e';
 		const isValid = ticketValidator.isValid(validCode);
 		expect(isValid).toBe(true);
+	});
+
+	it('should throw MissingParamError for falsy ticket code', () => {
+		const falsyCodes = [null, undefined, '', 0];
+		falsyCodes.forEach((code) => {
+			expect(() => ticketValidator.isValid(code)).toThrow(MissingParamError);
+		});
 	});
 });
