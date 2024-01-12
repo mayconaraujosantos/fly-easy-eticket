8d4502ce56358bbbc0e1e856e53a7059462b284d
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2024-01-12 01:37:34 -0300

    chore: remove prisma dependencie

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { TripScraper } from './infra/puppeteer/trip-scraper';
 
-// const code = '1cffdeb03abff1346e6148c6d46376c8e0eb9d6e';
 const code = '1cffdeb03abff1346e6148c6d46376c8e0eb9d6e';
 
 async function main() {
